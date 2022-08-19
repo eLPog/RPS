bb3cf062a590ffb2b6e0e7395d710243c777e3c6
LukasP <lukas.straubing@gmail.com>
2022-08-19 22:19:56 +0200

    all functionality added, games history added

--- src/App.js
+++ src/App.js
@@ -27,6 +27,9 @@ function App() {
   const [winner, setWinner] = useState(null);
   const [player1Score, setPlayer1Score] = useState(0);
   const [player2Score, setPlayer2Score] = useState(0);
+  const [roundFinished, setRoundFinished] = useState(false);
+  const [resultsHistory, setResultsHistory] = useState([]);
+  const [gameNumber, setGameNumber] = useState(1);
 
   const player1NameHandler = (e) => setPlayer1Name(e.target.value);
   const player2NameHandler = (e) => setPlayer2Name(e.target.value);
@@ -37,20 +40,37 @@ function App() {
   const player2ChoiceHandler = (value) => {
     setPlayer2Choice(value);
   };
+  function newGame() {
+    setRoundFinished(false);
+  }
   function checkWinner(choicePlayer1, choicePlayer2) {
     const player1 = choices.find((el) => el.emoji === choicePlayer1);
     const player2 = choices.find((el) => el.emoji === choicePlayer2);
+    const gameStats = {
+      winner: null,
+      selectedSignPlayer1: player1Choice,
+      selectedSignPlayer2: player2Choice,
+      gameID: Math.floor(Math.random() * 10000),
+      gameNumber,
+    };
     if (player1.beat === player2.name) {
       setWinner(player1Name);
       setPlayer1Score(((prevState) => prevState + 1));
+      gameStats.winner = player1Name;
     }
     if (player2.beat === player1.name) {
       setWinner(player2Name);
       setPlayer2Score(((prevState) => prevState + 1));
+      gameStats.winner = player2Name;
     }
     if (player1.name === player2.name) {
       setWinner('Draw');
+      gameStats.winner = 'draw';
     }
+    resultsHistory.push(gameStats);
+    setResultsHistory((prevState) => prevState);
+    setGameNumber((prevState) => prevState + 1);
+    setRoundFinished(true);
     setPlayer1Choice(null);
     setPlayer2Choice(null);
   }
@@ -67,7 +87,6 @@ function App() {
                 <input className="player" id="player1Name" onChange={player1NameHandler} />
               </div>
             ) : null}
-
             <span className="playerName">{player1Name}</span>
             {choices.map((el) => <button className="selection" key={el.id} onClick={() => player1ChoiceHandler(el.emoji)}>{el.emoji}</button>)}
           </div>
@@ -83,46 +102,69 @@ function App() {
             {choices.map((el) => <button className="selection" key={el.id} onClick={() => player2ChoiceHandler(el.emoji)}>{el.emoji}</button>)}
           </div>
         </div>
-        <div className="playZone">
-          <span className="instruction">
-            {player1Choice ? player2Name : player1Name}
-            {' '}
-            select your sign
-          </span>
-          {player1Choice && player2Choice ? <button onClick={() => checkWinner(player1Choice, player2Choice)}> Play </button> : null}
+        {player1Name && player2Name ? (
+          <>
+            <div className="playZone">
+              {!roundFinished && (!player1Choice || !player2Choice) ? (
+                <>
+                  {' '}
+                  <span className="nameToSelect">
+                    {player1Choice ? player2Name : player1Name}
+                  </span>
+                  <span className="instruction">
+                    select your sign
+                  </span>
+                </>
+              ) : null }
 
-          <h2>
-            The winner is
-            {winner}
-          </h2>
-        </div>
+              {player1Choice && player2Choice ? <button onClick={() => checkWinner(player1Choice, player2Choice)}> Play </button> : null}
+              {roundFinished && (
+              <>
+                <h2 className="winnerName">
+                  The winner is
+                  {winner}
+                </h2>
+                <button onClick={newGame}>Next game</button>
+              </>
 
-        <div className="results">
-          <div>
-            <span className="result-score">
-              {player1Name}
-              {' '}
-              score:
-              {' '}
-              {player1Score}
-            </span>
-            <span className="result-score">
-              {player1Choice}
-              {' '}
-            </span>
-          </div>
-          <div>
-            <span className="result-score">
-              {' '}
-              {player2Name}
-              {' '}
-              score:
-              {' '}
-              {player2Score}
-            </span>
-            <span className="result-score">{player2Choice}</span>
-          </div>
-        </div>
+              )}
+
+            </div>
+            <div className="results">
+              <span className="result-score">
+                {player1Name}
+                {' '}
+                score:
+                {' '}
+                {player1Score}
+              </span>
+              <span className="result-score">
+                {' '}
+                {player2Name}
+                {' '}
+                score:
+                {' '}
+                {player2Score}
+              </span>
+            </div>
+            <div className="history">
+              <ul className="historyList">
+                {resultsHistory.map((el) => (
+                  <li key={el.gameID}>
+                    Game number:
+                    {' '}
+                    {el.gameNumber}
+                    ,
+                    Winner:
+                    {' '}
+                    {el.winner}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </>
+        ) : null}
+
       </main>
     </div>
   );
