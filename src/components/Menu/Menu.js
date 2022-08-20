c7937c4087c42f996d2a1c52215d715083706095
LukasP <lukas.straubing@gmail.com>
2022-08-20 13:06:07 +0200

    hamburger menu added

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,18 +1,29 @@
 import './Menu.css';
+import { useState } from 'react';
 
 export function Menu() {
+  const [showMenu, setShowMenu] = useState(false);
+  function showMenuHandler() {
+    showMenu ? setShowMenu(false) : setShowMenu(true);
+  }
   return (
-    <div className="menu">
-      <nav className="nav">
-        <ul>
-          <li>
-            Reset history
-          </li>
-          <li>
-            Set names
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav className="menu">
+      <input type="checkbox" id="menuToggle" />
+      <label htmlFor="menuToggle" className="menuButtonContainer">
+        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
+        <div className="menuButton" onClick={showMenuHandler} />
+      </label>
+      {showMenu && (
+      <ul className="list">
+        <li>
+          Set new names
+        </li>
+        <li>
+          reset history
+        </li>
+      </ul>
+      )}
+
+    </nav>
   );
 }
