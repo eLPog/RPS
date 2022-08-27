78173a4197c52c5b92fe27f8b22c6a6c868ca7b9
LukasP <lukas.straubing@gmail.com>
2022-08-27 15:15:50 +0200

    interface for choices array created

--- src/interface/Interfaces.tsx
+++ src/interface/Interfaces.tsx
@@ -10,3 +10,9 @@ export interface LastGameInterface {
     player1:string,
     player2:string
 }
+export interface PlaySign {
+    name:string,
+    emoji:string,
+    beat: string[],
+    id:number
+}
